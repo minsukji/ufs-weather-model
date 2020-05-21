40699d7ac5594c28c2a2d659ca06815f97863ebd
MinsukJi-NOAA <minsuk.ji@noaa.gov>
2020-05-21 13:41:16 -0400

    Get FULL container ID in check_memory.sh

--- tests/check_memory.sh
+++ tests/check_memory.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-sleep 30
-containerID=$(docker ps --format "{{.ID}}")
+sleep 20
+containerID=$(docker ps -q --no-trunc)
+#containerID=$(docker ps --format "{{.ID}}")
 while [ -d /sys/fs/cgroup/memory/docker/${containerID} ] ; do
   sed -n 1p /sys/fs/cgroup/memory/docker/${containerID}/memory.stat
   sleep 5
