bde1077b4aa20d509258625dfa20aef3e0af34a7
MinsukJi-NOAA <minsuk.ji@noaa.gov>
2020-05-21 21:53:39 -0400

    12 proc mem stat

--- tests/check_memory.sh
+++ tests/check_memory.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-sleep 20
+sleep 10
 containerID=$(docker ps -q --no-trunc)
 #containerID=$(docker ps --format "{{.ID}}")
 while [ -d /sys/fs/cgroup/memory/actions_job/${containerID} ] ; do
-  sed -n 1p /sys/fs/cgroup/memory/actions_job/${containerID}/memory.stat
-  sleep 5
+  sed -n 2p /sys/fs/cgroup/memory/actions_job/${containerID}/memory.stat | awk '{printf "%15d\n", $2}' >>mem.txt
+  sleep 2
 done
