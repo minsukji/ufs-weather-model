90d09d479c55e9aa0dff77726bb31dbaaffea866
MinsukJi-NOAA <minsuk.ji@noaa.gov>
2021-04-05 14:23:54 -0400

    Check if repo is current

--- tests/ci/json_helper.py
+++ tests/ci/json_helper.py
@@ -24,6 +24,10 @@ def cancel_workflow(data):
 
   return wfs
 
+def get_pr_no(data, hs):
+    prn=[x["number"] for x in data if x["head"]["sha"]==hs]
+    return prn
+
 def main():
 
   if sys.argv[1]=="check_run":
@@ -34,6 +38,20 @@ def main():
     print(json.load(sys.stdin)["workflow_run"]["id"])
   elif sys.argv[1]=="get_trigger_br":
     print(json.load(sys.stdin)["workflow_run"]["head_branch"])
+  elif sys.argv[1]=="base_sha":
+    print(json.load(sys.stdin)["commit"]["sha"])
+  elif sys.argv[1]=="comp_sha";
+    print(json.load(sys.stdin)["sha"])
+  elif sys.argv[1]=="head_sha":
+    print(json.load(sys.stdin)["workflow_run"]["head_sha"]
+  elif sys.argv[1]=="get_pr":
+    prn =  get_pr_no(json.load(sys.stdin), sys.argv[2])
+    if len(prn)==0:
+      print("")
+    else
+      print(*prn)
+  elif sys.argv[1]=="pr_uid":
+    print(json.load(sys.stdin)["workflow_run"]["head_repository"]["owner"]["login"])
   elif sys.argv[1]=="cancel_workflow":
     data = json.load(sys.stdin)["workflow_runs"]
     wfs = cancel_workflow(data)
